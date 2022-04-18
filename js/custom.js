88303ef1f4fe4ef5e868555b7b43083b5ec3b841
Sajjad Ahmad <73050766+sajjadah964@users.noreply.github.com>
2022-04-18 21:58:06 +0500

    updated mobile menu

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,26 @@
 
+// $(document).ready(function () {
+//     $('#open-menu').click(function () {
+//         $('#myLinks').slideToggle();
+//     });
+// });
+var isMenuOpen=false;
 $(document).ready(function () {
-    $('#open-menu').click(function () {
+    $('.mobile-icon-wrapper').on ('click',function () {
+        $('.mobile-icon-wrapper').children('span').fadeToggle();
         $('#myLinks').slideToggle();
+        isMenuOpen=!isMenuOpen;
     });
 });
+$(window).on('resize',function() {
+    if ($(window).width() >767) {
+        $('#myLinks').show();  
+    }
+    else {
+        if (isMenuOpen) {
+            $('#myLinks').show();    
+        } else {
+            $('#myLinks').hide();  
+        }
+    }
+})
